d5c2f59ff26f0be3b8280f677e58ea9e90189ee7
Elias Ablan <eliasablan93@gmail.com>
2025-01-21 20:42:55 -0400

    hide pagination on error

--- src/app/(home)/_components/Pagination.tsx
+++ src/app/(home)/_components/Pagination.tsx
@@ -4,20 +4,26 @@ import React from "react";
 import { useCharactersContext } from "./CharactersContext";
 
 export default function Pagination() {
-  const { isLoading, page, setPage, pages } = useCharactersContext();
+  const { isLoading, page, setPage, pages, error } = useCharactersContext();
+
+  if (error) return;
 
   return (
     <div className="join mx-auto grid max-w-7xl grid-cols-2">
       <button
         onClick={() => setPage(page - 1)}
-        className={`btn btn-outline join-item ${(isLoading || page === 1) && "btn-disabled"}`}
+        className={`btn btn-outline join-item ${
+          (isLoading || page === 1) && "btn-disabled"
+        }`}
       >
         Previous page
       </button>
 
       <button
         onClick={() => setPage(page + 1)}
-        className={`btn btn-outline join-item ${(isLoading || page === pages) && "btn-disabled"}`}
+        className={`btn btn-outline join-item ${
+          (isLoading || page === pages) && "btn-disabled"
+        }`}
       >
         Next page
       </button>
